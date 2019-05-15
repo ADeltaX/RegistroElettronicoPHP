079195c30df9f23330011182cb1bdde9aeb2d8f7
kevkorani99 <50602084+kevkorani99@users.noreply.github.com>
2019-05-15 13:33:06 +0200

    Update Snippets.php
    - Modifica delle material Icons

--- functions/snippets.php
+++ functions/snippets.php
@@ -5,51 +5,51 @@ function StampaNavItems($tipoutente)
 	//APPELLO
 	if ($tipoutente == 1) //professore
 	  echo '<li class="nav-item"><a class="nav-link" href="/RegistroElettronicoPHP/pages/professori/appello.php">
-		<i class="material-icons">hearing</i><span>Appello</span></a></li>';
+		<i class="material-icons">pan_tool</i><span>Appello</span></a></li>';
 
 	//VOTI
 	if ($tipoutente == 2 || $tipoutente == 3) //genitore e studente
 	  echo '<li class="nav-item"><a class="nav-link" href="/RegistroElettronicoPHP/pages/comuni/voti.php">
-		<i class="material-icons"> accessible_forward</i><span>Voti</span></a></li>';
+		<i class="material-icons">sentiment_very_satisfied</i><span>Voti</span></a></li>';
 	else if ($tipoutente == 1) //professore
 	  echo '<li class="nav-item"><a class="nav-link" href="/RegistroElettronicoPHP/pages/professori/voti.php">
-		<i class="material-icons"> accessible_forward</i><span>Inserisci voti</span></a></li>';
+		<i class="material-icons">edit</i><span>Inserisci voti</span></a></li>';
 
 	//ASSENZE
 	if ($tipoutente == 2 || $tipoutente == 3) //genitore e studente
 	  echo '<li class="nav-item"><a class="nav-link" href="/RegistroElettronicoPHP/pages/comuni/assenze.php">
-		<i class="material-icons">vertical_split</i><span>Assenze</span></a></li>';
+		<i class="material-icons">clear</i><span>Assenze</span></a></li>';
 	else if ($tipoutente == 1) //professore
 	  echo '<li class="nav-item"><a class="nav-link" href="/RegistroElettronicoPHP/pages/professori/assenze.php">
-		<i class="material-icons">vertical_split</i><span>Gestione assenze</span></a></li>';
+		<i class="material-icons">clear</i><span>Gestione assenze</span></a></li>';
 
 	//COMUNICAZIONI - per tutti
 	echo '<li class="nav-item"><a class="nav-link" href="/RegistroElettronicoPHP/pages/comuni/comunicazioni.php">
-	  <i class="material-icons">vertical_split</i><span>Comunicazioni</span></a></li>';
+	  <i class="material-icons">comment</i><span>Comunicazioni</span></a></li>';
 		
 	//MATERIE e PROFESSORI (+ classi per professore)
 	if ($tipoutente == 2 || $tipoutente == 3) //genitore e studente
 	  echo '<li class="nav-item"><a class="nav-link" href="/RegistroElettronicoPHP/pages/comuni/materieeprofessori.php">
-		<i class="material-icons">vertical_split</i><span>Materie e Professori</span></a></li>';
+		<i class="material-icons">supervisor_account</i><span>Materie e Professori</span></a></li>';
 	else if ($tipoutente == 1) //professore
 	  echo '<li class="nav-item"><a class="nav-link" href="/RegistroElettronicoPHP/pages/professori/materieclassi.php">
-		<i class="material-icons">vertical_split</i><span>Materie e Classi</span></a></li>';
+		<i class="material-icons">book</i><span>Materie e Classi</span></a></li>';
 
 	//ORARIO
 	if ($tipoutente == 2 || $tipoutente == 3) //genitore e studente
 	  echo '<li class="nav-item"><a class="nav-link" href="/RegistroElettronicoPHP/pages/comuni/orario.php">
-		<i class="material-icons">vertical_split</i><span>Orario lezioni</span></a></li>';
+		<i class="material-icons">verticalquery_builder_split</i><span>Orario lezioni</span></a></li>';
 	else if ($tipoutente == 1) //professore
 	  echo '<li class="nav-item"><a class="nav-link" href="/RegistroElettronicoPHP/pages/professori/orario.php">
-		<i class="material-icons">vertical_split</i><span>Orario</span></a></li>';
+		<i class="material-icons">query_builder</i><span>Orario</span></a></li>';
 
 	//NOTE e ANNOTAZIONI
 	if ($tipoutente == 2) //genitore
 	  echo '<li class="nav-item"><a class="nav-link" href="/RegistroElettronicoPHP/pages/genitori/note.php">
-		<i class="material-icons">vertical_split</i><span>Note disciplinari e annotazioni</span></a></li>';
+		<i class="material-icons">thumb_down_alt</i><span>Note disciplinari e annotazioni</span></a></li>';
 	else if ($tipoutente == 1) //professore
 	  echo '<li class="nav-item"><a class="nav-link" href="/RegistroElettronicoPHP/pages/professori/note.php">
-		<i class="material-icons">vertical_split</i><span>Inserisci note</span></a></li>';
+		<i class="material-icons">thumb_down_alt</i><span>Inserisci note</span></a></li>';
 }
 
 function StampaFooter()
